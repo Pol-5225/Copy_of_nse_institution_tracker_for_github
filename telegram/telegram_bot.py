0bf993f8603658febe34921b304878c514cf3db3
sangram <sangrampol5225@gmail.com>
2026-03-11 16:33:12 +0530

    Testing for the telegram python file

--- telegram/telegram_bot.py
+++ telegram/telegram_bot.py
@@ -1,14 +1,18 @@
+import os
 import requests
 
-
-BOT_TOKEN = "BOT_TOKEN"
-CHAT_ID = "CHAT_ID"
+# Read values from GitHub secrets (environment variables)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 
 def send_message(message):
     """
     Send message to Telegram
     """
+
+    print("BOT_TOKEN loaded:", BOT_TOKEN is not None)
+    print("CHAT_ID loaded:", CHAT_ID is not None)
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
@@ -19,7 +23,11 @@ def send_message(message):
 
     response = requests.post(url, data=payload)
 
+    print("Telegram API response:")
+    print(response.text)
+
     return response.json()
+
 
 if __name__ == "__main__":
     send_message("Test message from NSE institutional tracker 🚀")
