a1ebd637f7774364d6dad1d70188cf22ca2bbdd5
sangram <sangrampol5225@gmail.com>
2026-03-11 18:06:18 +0530

    DEBUGING Added extra group chat ID to receive ALERTS on GROUP on Telegram

--- telegram/telegram_bot.py
+++ telegram/telegram_bot.py
@@ -2,37 +2,48 @@ import os
 import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = [
+
+CHAT_IDS = [
     os.getenv("CHAT_ID"),        # your personal chat
     os.getenv("GROUP_CHAT_ID")   # telegram group
 ]
+
 # Safety check
-if not BOT_TOKEN or not CHAT_ID:
-    print("⚠ Telegram credentials missing. BOT_TOKEN or CHAT_ID not found.")
+if not BOT_TOKEN:
+    print("⚠ Telegram BOT_TOKEN missing.")
 
 def send_message(message):
     print("BOT_TOKEN loaded:", bool(BOT_TOKEN))
-    print("CHAT_ID loaded:", bool(CHAT_ID))
+    print("CHAT_IDS loaded:", CHAT_IDS)
 
-    if not BOT_TOKEN or not CHAT_ID:
-        raise RuntimeError("Missing BOT_TOKEN or CHAT_ID environment variable")
+    if not BOT_TOKEN:
+        raise RuntimeError("Missing BOT_TOKEN environment variable")
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML"
-    }
 
-    resp = requests.post(url, data=payload, timeout=20)
-    print("Telegram API status:", resp.status_code)
-    print("Telegram API response:", resp.text)
+    for chat_id in CHAT_IDS:
 
-    data = resp.json()
-    if not data.get("ok"):
-        raise RuntimeError(f"Telegram error: {data}")
+        if not chat_id:
+            continue
 
-    return data
+        payload = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "HTML"
+        }
+
+        resp = requests.post(url, data=payload, timeout=20)
+
+        print("Sending to:", chat_id)
+        print("Telegram API status:", resp.status_code)
+        print("Telegram API response:", resp.text)
+
+        data = resp.json()
+        if not data.get("ok"):
+            raise RuntimeError(f"Telegram error: {data}")
+
+    return True
+
 
 if __name__ == "__main__":
     send_message("Test message from NSE institutional tracker 🚀")
